f1e7a61214ed5557d94e40f59845a82e10551dc3
SaubhagyaSapkota <saubhagyasapkota444@gmail.com>
2025-07-31 13:09:56 +0545

    custom routing, env based config

--- nodejs/simple_server/index.js
+++ nodejs/simple_server/index.js
@@ -1,12 +1,39 @@
 const http = require("http");
 const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
 const server = http.createServer((req, res) => {
-  let parseUrl = url.parse(req.url, true);
-  console.log(parseUrl);
-  res.end("Hello Everyone");
+  let parserUrl = url.parse(req.url, true);
+
+  let path = parserUrl.pathname;
+
+  let trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  let headers = req.headers;
+
+  let method = req.method.toLowerCase();
+  let queryStringObject = parserUrl.query;
+
+  let decoder = new StringDecoder("utf-8");
+  let buffer = "";
+  req.on("data", (data) => {
+    buffer += decoder.write(data);
+  });
+  req.on("end", () => {
+    buffer += decoder.end();
+    res.end(
+      "Received request on path: " +
+        trimmedPath +
+        " with http method: " +
+        method +
+        " and this query string: " +
+        JSON.stringify(queryStringObject) +
+        "with data: " +
+        buffer
+    );
+  });
 });
 
 server.listen(3000, () => {
-  console.log("server is running on port: 3000");
+  console.log("Server is running now");
 });
